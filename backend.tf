355441ffe57f8ff91a769b4e8db53922cc001235
Daazd <zoomie067@yahoo.com>
2024-04-22 21:41:49 -0700

    changes

--- backend.tf
+++ backend.tf
@@ -1,12 +1,4 @@
 terraform {
-  backend "remote" {
-    hostname     = "app.terraform.io"
-    organization = "Vault1"
-    workspaces {
-      name = "Automation-learning"
-    }
-  }
-
   cloud {
     organization = "Vault1"
 
