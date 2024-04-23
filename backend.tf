2361b11324b650ede8d110bc6f7b75d1c5c86245
Daazd <zoomie067@yahoo.com>
2024-04-22 21:22:49 -0700

    changes

--- backend.tf
+++ backend.tf
@@ -3,7 +3,7 @@ terraform {
     hostname     = "app.terraform.io"
     organization = "Vault1"
     workspaces {
-      name = "learn-terraform-circleci"
+      prefix = "Automation-learning"
     }
   }
 }
