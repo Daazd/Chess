c96f5219d963dfb8e4401aff499bb41728d45963
Daazd <zoomie067@yahoo.com>
2024-04-22 21:31:27 -0700

    changes

--- backend.tf
+++ backend.tf
@@ -6,4 +6,15 @@ terraform {
       prefix = "Automation-learning"
     }
   }
+
+  cloud {}
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.4.0"
+    }
+  }
+
+  required_version = ">= 1.2.0"
 }
